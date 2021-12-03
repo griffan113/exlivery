846775c83f619bd138490bf8800ce842018e54ce
Eliel <elielmichel@gmail.com>
2021-12-03 16:41:09 -0300

    chore: create Item validation

---
+++ lib/orders/item.ex
@@ -0,0 +1,46 @@
+defmodule Exlivery.Orders.Item do
+  @categories [
+    :pizza,
+    :hamburguer,
+    :carne,
+    :prato_feito,
+    :japonesa,
+    :sobremesa
+  ]
+
+  @keys [
+    :description,
+    :category,
+    :unity_price,
+    :quantity
+  ]
+
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(description, category, unity_price, quantity)
+      when quantity > 0 and category in @categories do
+    unity_price
+    |> Decimal.cast()
+    |> build_item(description, category, quantity)
+  end
+
+  def build(_description, _category, _unity_price, _quantity),
+    do: {:error, "Invalid parameters"}
+
+  # {:ok} -> Returned by Decimal
+  defp build_item({:ok, unity_price}, description, category, quantity) do
+    {:ok,
+     %__MODULE__{
+       description: description,
+       category: category,
+       unity_price: unity_price,
+       quantity: quantity
+     }}
+  end
+
+  # :error -> Returned by Decimal
+  defp build_item(:error, _description, _unity_price, _quantity),
+    do: {:error, "Invalid price"}
+end
